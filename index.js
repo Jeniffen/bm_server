bd8239d72fb22a9f6dd8e4b1880dc13f01835d6d
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2021-02-02 21:51:45 +0100

    Implement working google OAuth flow (#1)

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+
+require("./services/mongoose");
 require("./services/passport");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = 5000;
---
+++ services/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+require("../models/User");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
